09ad76ec680c6252191ca8e339cd94011bcbf208
Zahnen Garner <zahnen@gmail.com>
2020-12-15 14:29:01 -0800

    add structure for Views & Controller
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

---
+++ Parcels/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Parcels.Models;
+
+namespace Parcels.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+  }
+}
