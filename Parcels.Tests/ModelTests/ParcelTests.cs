c976a07ea9d326bf21a9e0683a7e9ef72971d280
Zahnen Garner <zahnen@gmail.com>
2020-12-15 11:11:05 -0800

    add second test, pass second test
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- Parcels.Tests/ModelTests/ParcelTests.cs
+++ Parcels.Tests/ModelTests/ParcelTests.cs
@@ -9,8 +9,19 @@ namespace Parcels.Tests
     [TestMethod]
     public void ParcelConstructor_CreateAnInstanceOfParcel_Parcel()
     {
-      Parcel newParcel = new Parcel();
+      Parcel newParcel = new Parcel(2,2,2,2);
       Assert.AreEqual(typeof(Parcel), newParcel.GetType());
+    }
+    [TestMethod]
+    public void ParcelConstructor_ReturnsParcelDimensions_Int()
+    {
+      int testParcelLength = 5;
+      Parcel newParcel = new Parcel(testParcelLength,7,3,4);
+      int confirmParcelLength = 5;
+      Assert.AreEqual(confirmParcelLength, newParcel.Length);
+
     }
   }
 }
+
+
--- Parcels/Models/Parcels.cs
+++ Parcels/Models/Parcels.cs
@@ -6,5 +6,13 @@ namespace Parcels.Models
     public int Width { get; set; }
     public int Height { get; set; }
     public int Weight { get; set; }
+
+    public Parcel(int length, int width, int height, int weight)
+    {
+      Length = length;
+      Width = width;
+      Height = height;
+      Weight = weight;
+    }
   }
 }
