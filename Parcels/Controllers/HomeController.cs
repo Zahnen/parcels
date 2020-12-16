4bdfc942d676d31ea50cbe32685da77b16b56810
Zahnen Garner <zahnen@gmail.com>
2020-12-15 16:14:20 -0800

    add form, attempt to pass form data to index via controller
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- Parcels/Controllers/HomeController.cs
+++ Parcels/Controllers/HomeController.cs
@@ -12,5 +12,20 @@ namespace Parcels.Controllers
       return View();
     }
 
+    [HttpGet("/parcels/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/parcels")]
+    public ActionResult Create(int length, int width, int height, int weight)
+    {
+      Parcel newParcel  = new Parcel(length, width, height, weight);
+      double volume = newParcel.Volume(); 
+      newParcel.Height = height;
+      double cost = newParcel.CostToShip(volume);
+      return RedirectToAction("Index");
+    }
   }
 }
